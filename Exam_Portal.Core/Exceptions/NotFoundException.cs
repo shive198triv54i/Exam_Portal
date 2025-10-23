65604cf5f07653c6d7708a0575d1178c6fb28c0d
shivesh.trivedi <shivesh.trivedi@intimetec.com>
2025-10-23 10:54:47 +0530

    feat: adding custom exception

---
+++ Exam_Portal.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Exam_Portal.Core.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
---
+++ Exam_Portal.Core/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,7 @@
+﻿namespace Exam_Portal.Core.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException(string message) : base(message) { }
+    }
+}
